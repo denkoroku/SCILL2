d28c78a0c6cfdc161f1e2e4a8d1df030a1b2c170
User <user@users-mbp.home>
2020-03-12 15:46:50 +0000

    added new front page file and showing list of events

---
+++ front-page.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * The template for displaying all pages
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages
+ * and that other 'pages' on your WordPress site may use a
+ * different template.
+ *
+ * @link https://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package WP_Bootstrap_Starter
+ */
+
+get_header(); ?>
+
+	<section id="primary" class="content-area col-sm-12 col-lg-8">
+		<main id="main" class="site-main" role="main">
+
+            <?php 
+                $homepageEvents = new WP_Query(array(
+                    'posts_per_page' => 2,
+                    'post_type' => 'event'
+                ));
+
+			while ($homepageEvents->have_posts()) {
+                $homepageEvents->the_post(); ?>
+                <li><?php the_title(); ?></li>
+                <?php }
+
+			?>
+
+		</main><!-- #main -->
+	</section><!-- #primary -->
+
+<?php
+get_sidebar();
+get_footer();
