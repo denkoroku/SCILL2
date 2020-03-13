0397031dcf33dce27eb9716457331c49ec72ebb2
User <user@users-mbp.home>
2020-03-13 12:19:59 +0000

    added event permalinks and updated wordpress permalink structure

--- front-page.php
+++ front-page.php
@@ -25,7 +25,8 @@ get_header(); ?>
 
 			while ($homepageEvents->have_posts()) {
                 $homepageEvents->the_post(); ?>
-                <li><?php the_title(); ?></li>
+                <h5><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h5>
+                <p><?php echo wp_trim_words(get_the_content(), 15)?> <a href="<?php the_permalink(); ?>">Learn more</a></p>
                 <?php }
 
 			?>
