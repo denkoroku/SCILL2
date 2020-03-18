92c7f90ad93864af3217a77c5163d0b7a96d31a2
User <user@users-mbp.home>
2020-03-18 15:19:55 +0000

    added past events page with working padgination links

---
+++ page-past-events.php
@@ -0,0 +1,69 @@
+<?php
+/**
+ * The template for displaying archive pages
+ *
+ * @link https://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package WP_Bootstrap_Starter
+ */
+
+get_header(); ?>
+
+	<section id="primary" class="content-area col-sm-12 col-lg-8">
+		<main id="main" class="site-main" role="main">
+
+        <div class="card-group">
+        <?php 
+                $today = date('Ymd');
+                $pastEvents = new WP_Query(array(
+                    'paged' => get_query_var('paged', 1),
+                    'post_type' => 'event',
+                    'meta_key' => 'event_date',
+                    'orderby' => 'meta_value_num',
+                    'order' => 'ASC',
+                    'meta_query' => array(
+                        array(
+                            'key' => 'event_date',
+                            'compare' => '<',
+                            'value' => $today,
+                            'type' => 'numeric'
+                        )
+                    )
+                ));
+
+			while ($pastEvents->have_posts()) {
+                $pastEvents->the_post(); ?>
+    <div class="col-12">
+        <div class="card mb-2">
+            <img class="card-img-top" src="<?php echo $featured_img_url ?>" alt="">
+            <div class="card-body">
+            <?php $eventDate = new DateTime(get_field('event_date')); ?>
+                <div class= "event-date">
+                    <p id="date-number"><?php echo $eventDate->format('d')?></p><span id="date-month"><?php 
+                        echo $eventDate->format('M');
+                    ?></span>
+                </div>
+                <h4 class="card-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h4>
+                <p class="card-text"><?php if (has_excerpt()) {
+                    echo get_the_excerpt();
+                } else {
+                    echo wp_trim_words(get_the_content(), 15);
+                } ?>
+                </p>
+                <a href="<?php the_permalink( ) ?>" class="solid-btn">Read More</a>
+            </div>
+        </div>
+    </div>
+            <?php }    
+            echo paginate_links(array(
+                'total' => $pastEvents->max_num_pages
+            ));
+         ?>
+</div>
+
+		</main><!-- #main -->
+	</section><!-- #primary -->
+
+<?php
+get_sidebar();
+get_footer();
