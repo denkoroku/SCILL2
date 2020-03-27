e08de7334b427b3000867a1258ce7e4a686ba724
Cath Riddoch <cathriddoch@gmail.com>
2020-03-27 14:23:16 +0000

    sorts icons

--- front-page.php
+++ front-page.php
@@ -13,14 +13,14 @@ get_header(); ?>
                     <h3 class="text-center mb-5" >SCILL is a charity registered in Scotland and dedicated to helping parents and professionals caring for children with additional support needs.</h3>
                 <div class="homepage-icons">
                 <div id="cup">
-                    <img src="<?php echo get_theme_file_uri('assets/images/500px-cup-icon.png') ?>"> <div class="icon-descrip">support sessions</div>
+                    <img src="<?php echo get_theme_file_uri('assets/images/final-cup-icon.png') ?>"> <div class="icon-descrip">support sessions</div>
                 </div>
                 <div id="child">
-                    <img src="<?php echo get_theme_file_uri('assets/images/500px-child-icon.png') ?>">
+                    <img src="<?php echo get_theme_file_uri('assets/images/final-child-icon.png') ?>">
                     <p class="icon-descrip">children's activities</p>
                 </div>
                 <div id="talk">
-                    <img src="<?php echo get_theme_file_uri('assets/images/500px-talk-icon.png') ?>">
+                    <img src="<?php echo get_theme_file_uri('assets/images/final-talk-icon.png') ?>">
                     <p class="icon-descrip">workshops and talks</p>
                 </div>
             </div>
